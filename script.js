ff82d01b5fe565da92b4ea7da6aefa18fb22ab5c
gabedavaa <110985736+gabedavaa@users.noreply.github.com>
2023-02-18 19:33:05 +0400

    update js

--- script.js
+++ script.js
@@ -11,58 +11,58 @@ const cityName = document.querySelector(".city-name");
 const weatherContainer = document.querySelector(".weather-container");
 const loadingErrorElement = document.querySelector(".loading-error");
 
-const apiKey = "464e32adc6b9045a369e9406cb9a663f";
+let weather = {
+  apiKey: "464e32adc6b9045a369e9406cb9a663f",
+  fetchWeather: function (city) {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.apiKey}&units=metric`
+    )
+      .then((response) => {
+        if (!response.ok) {
+          weatherContainer.style.display = "none";
+          alert("No weather found.");
+          throw new Error("No weather found.");
+        }
+        return response.json();
+      })
+      .then((data) => this.displayWeather(data));
+  },
+
+  displayWeather: function (data) {
+    weatherContainer.style.display = "block";
+    loadingErrorElement.style.display = "none";
+    cityName.textContent = `Weather in ${data.name}`;
+
+    weatherTemp.textContent = `${Math.round(data.main.temp)}℃`;
+
+    weatherIcon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+
+    description.textContent = `${data.weather[0].description}`;
+
+    humidityElement.textContent = `Humidity: ${Number.parseFloat(
+      data.main.humidity
+    ).toFixed(2)} %`;
+
+    windSpeed.textContent = `Wind speed: ${Number.parseFloat(
+      data.wind.speed
+    ).toFixed(2)} km/h`;
+  },
+
+  search: function () {
+    this.fetchWeather(searchBar.value);
+  },
+};
 
 searchBar.addEventListener("keyup", (e) => {
   if (e.target.value && e.key === "Enter") {
-    getWeather(e.target.value);
+    weather.search();
   }
 });
 
 buttonCity.addEventListener("click", function () {
-  getWeather(searchBar.value);
+  buttonCity.disabled = true;
+  weather.search();
+  buttonCity.disabled = false;
 });
 
-async function getWeather(city) {
-  try {
-    buttonCity.disabled = true;
-    loadingErrorElement.textContent = "Loading...";
-
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
-    const response = await fetch(apiUrl);
-    const data = await response.json();
-    console.log(data);
-
-    if (data) {
-      console.log("dv");
-      //city name
-      cityName.textContent = `Weather in ${data.name}`;
-
-      // weather temp
-      weatherTemp.textContent = `${Math.round(data.main.temp)}℃`;
-
-      // Icon
-      weatherIcon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
-
-      // Description
-      description.textContent = `${data.weather[0].description}`;
-
-      // Humidity
-      humidityElement.textContent = `Humidity: ${Number.parseFloat(
-        data.main.humidity
-      ).toFixed(2)} %`;
-
-      // Wind Speed
-      windSpeed.textContent = `Wind speed: ${Number.parseFloat(
-        data.wind.speed
-      ).toFixed(2)} km/h`;
-
-      weatherContainer.style.display = "block";
-      loadingErrorElement.style.display = "none";
-    }
-  } catch (error) {
-    console.log("error");
-    loadingErrorElement.textContent = "Something went wrong";
-  }
-}
+weather.fetchWeather("tbilisi");
