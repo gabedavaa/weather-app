35cbe63197408288734c74972a33551bb2c67e94
gabedavaa <110985736+gabedavaa@users.noreply.github.com>
2023-02-18 11:48:38 +0400

    js commit

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 "use strict";
 
 const buttonCity = document.getElementById("button-city");
+const searchBar = document.querySelector("search-bar");
 
 const apiKey = "464e32adc6b9045a369e9406cb9a663f";
 
 buttonCity.addEventListener("click", function () {
+  console.log(searchBar.textContent);
   console.log("helloo");
   const url = `https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}`;
 });
