43591f23f5a30e986937efa78c7520556d26edeb
gabedavaa <110985736+gabedavaa@users.noreply.github.com>
2023-02-18 16:57:30 +0400

    js commit

--- script.js
+++ script.js
@@ -2,7 +2,14 @@
 
 const buttonCity = document.getElementById("button-city");
 const searchBar = document.querySelector(".search-bar");
-const windSpeed = document.querySelector("wind");
+const windSpeed = document.querySelector(".wind");
+const humidityElement = document.querySelector(".humidity");
+const description = document.querySelector(".description");
+const weatherIcon = document.querySelector(".weather-icon");
+const weatherTemp = document.querySelector(".temp");
+const cityName = document.querySelector(".city-name");
+const weatherContainer = document.querySelector(".weather-container");
+const loadingErrorElement = document.querySelector(".loading-error");
 
 const apiKey = "464e32adc6b9045a369e9406cb9a663f";
 
@@ -17,13 +24,45 @@ buttonCity.addEventListener("click", function () {
 });
 
 async function getWeather(city) {
-  console.log(58);
-  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+  try {
+    buttonCity.disabled = true;
+    loadingErrorElement.textContent = "Loading...";
 
-  const response = await fetch(apiUrl);
-  const data = await response.json();
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-  // windSpeed.value = Number(data.wind.speed);
-  console.log(data);
-  console.log(Number(data.wind.speed));
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    console.log(data);
+
+    if (data) {
+      console.log("dv");
+      //city name
+      cityName.textContent = `Weather in ${data.name}`;
+
+      // weather temp
+      weatherTemp.textContent = `${Math.round(data.main.temp)}℃`;
+
+      // Icon
+      weatherIcon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+
+      // Description
+      description.textContent = `${data.weather[0].description}`;
+
+      // Humidity
+      humidityElement.textContent = `Humidity: ${Number.parseFloat(
+        data.main.humidity
+      ).toFixed(2)} %`;
+
+      // Wind Speed
+      windSpeed.textContent = `Wind speed: ${Number.parseFloat(
+        data.wind.speed
+      ).toFixed(2)} km/h`;
+
+      weatherContainer.style.display = "block";
+      loadingErrorElement.style.display = "none";
+    }
+  } catch (error) {
+    console.log("error");
+    loadingErrorElement.textContent = "Something went wrong";
+  }
 }
