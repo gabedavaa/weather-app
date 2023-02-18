ba51b0086bd90f8955b4a7030b037fc006038463
gabedavaa <110985736+gabedavaa@users.noreply.github.com>
2023-02-18 14:27:02 +0400

    getting api

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 "use strict";
 
 const buttonCity = document.getElementById("button-city");
-const searchBar = document.querySelector("search-bar");
+const searchBar = document.querySelector(".search-bar");
+const windSpeed = document.querySelector("wind");
 
 const apiKey = "464e32adc6b9045a369e9406cb9a663f";
 
-buttonCity.addEventListener("click", function () {
-  console.log(searchBar.textContent);
-  console.log("helloo");
-  const url = `https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}`;
+searchBar.addEventListener("keyup", (e) => {
+  if (e.target.value && e.key === "Enter") {
+    getWeather(e.target.value);
+  }
 });
+
+buttonCity.addEventListener("click", function () {
+  getWeather(searchBar.value);
+});
+
+async function getWeather(city) {
+  console.log(58);
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+  const response = await fetch(apiUrl);
+  const data = await response.json();
+
+  // windSpeed.value = Number(data.wind.speed);
+  console.log(data);
+  console.log(Number(data.wind.speed));
+}
